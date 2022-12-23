c9b4db3736154fd10c0d9f84ea102c41a0132f68
Mehmet Gul <mehmet@clarusway.com>
2022-12-22 20:41:59 -0500

    This is my  commit

--- src/main/java/org/example/LoginTest.java
+++ src/main/java/org/example/LoginTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class LoginTest {
     public static void main(String[] args) {
-        System.out.println();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
